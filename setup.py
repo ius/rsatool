a8d866e2d6b6d23c7df45f59355cf672c9549b29
Hanno Böck <hanno@gentoo.org>
2022-01-09 10:47:46 +0100

    Make codingstyle more consistent

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@
 from setuptools import setup
 
 setup(name='rsatool',
-        version='1.0',
-        description='rsatool can be used to calculate RSA and RSA-CRT parameters',
-        author='Joerie de Gram',
-        author_email='j.de.gram@gmail.com',
-        url='https://github.com/ius/rsatool',
-        install_requires=['gmpy2', 'pyasn1'],
-        scripts=['rsatool.py']
-        )
+      version='1.0',
+      description='rsatool can be used to calculate RSA and RSA-CRT parameters',
+      author='Joerie de Gram',
+      author_email='j.de.gram@gmail.com',
+      url='https://github.com/ius/rsatool',
+      install_requires=['gmpy2', 'pyasn1'],
+      scripts=['rsatool.py']
+      )
