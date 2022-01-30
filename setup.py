3a2ca02cdca8f210b0aa42727e52661acb394492
Joerie de Gram <j.de.gram@gmail.com>
2022-01-30 22:16:36 +0100

    setup.py: fix formatting (closes #18)

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
-setup(name='rsatool',
-      version='1.0',
-      description='rsatool calculates RSA and RSA-CRT parameters',
-      author='Joerie de Gram',
-      author_email='j.de.gram@gmail.com',
-      url='https://github.com/ius/rsatool',
-      install_requires=['gmpy2', 'pyasn1'],
-      scripts=['rsatool.py']
+setup(
+    name='rsatool',
+    version='1.0',
+    description='rsatool calculates RSA and RSA-CRT parameters',
+    author='Joerie de Gram',
+    author_email='j.de.gram@gmail.com',
+    url='https://github.com/ius/rsatool',
+    install_requires=['gmpy2', 'pyasn1'],
+    scripts=['rsatool.py']
 )
